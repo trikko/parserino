8dfc5475b26adf45331a5d3fa24152cdbed4137e
Andrea Fontana <trikkuz@gmail.com>
2022-10-18 17:17:25 +0200

    Trying to support windows

--- build_lexbor_lib.d
+++ build_lexbor_lib.d
@@ -5,86 +5,93 @@ import std;
 int main()
 {
 
-    enum commit = "447cec843e0017a4b0924e9f0d8eff52824d1fa4";
-
-    // Ok, already done.
-    if (exists("liblexbor_parserino.a"))
+    version(WINDOWS)
+    {
         return 0;
-
-    // Temp directory
-    auto path = tempDir.buildPath(tempDir(), "lexbor_build", "lexbor_bundle.zip");
-    mkdirRecurse(dirName(path));
-
-    auto cmake = executeShell("cmake --help");
-
-    if (cmake.status != 0)
-    {
-        version(linux) stderr.writeln("Please install cmake on your system. For example on debian/ubuntu/... : `sudo apt install cmake`");
-        version(OSX) stderr.writeln("Please install cmake on your system. Try: `brew install cmake`");
-        return 1;
-    }
-
-    stderr.writeln;
-    stderr.writeln("-------------------------");
-    stderr.writeln("-- One time deps build --");
-    stderr.writeln("-------------------------");
-    stderr.writeln;
-
-    stderr.writeln("- Temp. directory: ", path); stderr.flush();
-
-    // Download bundle
-    stderr.writeln("- Getting zip bundle..."); stderr.flush();
-    download("https://github.com/lexbor/lexbor/archive/" ~ commit ~ ".zip", path);
-
-    // Unzip
-    stderr.writeln("- Unzipping bundle..."); stderr.flush();
-    ZipArchive zip = new ZipArchive(path.read);
-
-    foreach(string amPath, ref ArchiveMember am; zip.directory)
-    {
-        auto relPath = amPath;
-        auto filePath = tempDir.buildPath(tempDir(), "lexbor_build", relPath);
-        mkdirRecurse(dirName(filePath));
-
-        zip.expand(am);
-
-        if (!filePath.endsWith("/") && !filePath.endsWith("\\"))
-            std.file.write(filePath, am.expandedData());
-    }
-
-    // Compiling
-    stderr.writeln("- Compiling... (please wait, this will not be done anymore!)"); stderr.flush();
-
-    string cmakeopt;
-    version(OSX) cmakeopt = " -DCMAKE_OSX_DEPLOYMENT_TARGET=10.10 ";
-
-    auto res = executeShell("cd " ~ dirName(path).buildPath("lexbor-" ~ commit) ~ " && cmake . -DLEXBOR_BUILD_TESTS=OFF -DLEXBOR_BUILD_EXAMPLES=OFF -DLEXBOR_BUILD_SEPARATELY=OFF -DLEXBOR_BUILD_SHARED=OFF -DLEXBOR_BUILD_STATIC=ON " ~ cmakeopt ~ " && make");
-
-    stderr.writeln("- Waiting for result..."); stderr.flush();
-    auto outputFile = dirName(path).buildPath("lexbor-" ~ commit, "liblexbor_static.a");
-
-    bool done = true;
-
-    SysTime tm = Clock.currTime();
-    while(!outputFile.exists && done)
-    {
-        import core.thread;
-        Thread.sleep(150.dur!"msecs");
-
-        if (Clock.currTime() - tm > 5.dur!"seconds")
-            done = false;
-    }
-
-    if (done)
-    {
-        stderr.writeln("--- DONE ---\n");
-        copy(dirName(path).buildPath("lexbor-" ~ commit, "liblexbor_static.a"), "liblexbor_parserino.a");
     }
     else
     {
-        stderr.writeln("--- BUILD FAIL ---\nCompiler output:\n", res.output);
-        return 2;
-    }
+        enum commit = "447cec843e0017a4b0924e9f0d8eff52824d1fa4";
 
-    return 0;
+        // Ok, already done.
+        if (exists("liblexbor_parserino.a"))
+            return 0;
+
+        // Temp directory
+        auto path = tempDir.buildPath(tempDir(), "lexbor_build", "lexbor_bundle.zip");
+        mkdirRecurse(dirName(path));
+
+        auto cmake = executeShell("cmake --help");
+
+        if (cmake.status != 0)
+        {
+            version(linux) stderr.writeln("Please install cmake on your system. For example on debian/ubuntu/... : `sudo apt install cmake`");
+            version(OSX) stderr.writeln("Please install cmake on your system. Try: `brew install cmake`");
+            return 1;
+        }
+
+        stderr.writeln;
+        stderr.writeln("-------------------------");
+        stderr.writeln("-- One time deps build --");
+        stderr.writeln("-------------------------");
+        stderr.writeln;
+
+        stderr.writeln("- Temp. directory: ", path); stderr.flush();
+
+        // Download bundle
+        stderr.writeln("- Getting zip bundle..."); stderr.flush();
+        download("https://github.com/lexbor/lexbor/archive/" ~ commit ~ ".zip", path);
+
+        // Unzip
+        stderr.writeln("- Unzipping bundle..."); stderr.flush();
+        ZipArchive zip = new ZipArchive(path.read);
+
+        foreach(string amPath, ref ArchiveMember am; zip.directory)
+        {
+            auto relPath = amPath;
+            auto filePath = tempDir.buildPath(tempDir(), "lexbor_build", relPath);
+            mkdirRecurse(dirName(filePath));
+
+            zip.expand(am);
+
+            if (!filePath.endsWith("/") && !filePath.endsWith("\\"))
+                std.file.write(filePath, am.expandedData());
+        }
+
+        // Compiling
+        stderr.writeln("- Compiling... (please wait, this will not be done anymore!)"); stderr.flush();
+
+        string cmakeopt;
+        version(OSX) cmakeopt = " -DCMAKE_OSX_DEPLOYMENT_TARGET=10.10 ";
+
+        auto res = executeShell("cd " ~ dirName(path).buildPath("lexbor-" ~ commit) ~ " && cmake . -DLEXBOR_BUILD_TESTS=OFF -DLEXBOR_BUILD_EXAMPLES=OFF -DLEXBOR_BUILD_SEPARATELY=OFF -DLEXBOR_BUILD_SHARED=OFF -DLEXBOR_BUILD_STATIC=ON " ~ cmakeopt ~ " && make");
+
+        stderr.writeln("- Waiting for result..."); stderr.flush();
+        auto outputFile = dirName(path).buildPath("lexbor-" ~ commit, "liblexbor_static.a");
+
+        bool done = true;
+
+        SysTime tm = Clock.currTime();
+        while(!outputFile.exists && done)
+        {
+            import core.thread;
+            Thread.sleep(150.dur!"msecs");
+
+            if (Clock.currTime() - tm > 5.dur!"seconds")
+                done = false;
+        }
+
+        if (done)
+        {
+            stderr.writeln("--- DONE ---\n");
+            copy(dirName(path).buildPath("lexbor-" ~ commit, "liblexbor_static.a"), "liblexbor_parserino.a");
+        }
+        else
+        {
+            stderr.writeln("--- BUILD FAIL ---\nCompiler output:\n", res.output);
+            return 2;
+        }
+
+        return 0;
+    }
 }
--- source/parserino/c/lexbor.d
+++ source/parserino/c/lexbor.d
@@ -3,89 +3,193 @@ import core.stdc.stdint;
 
 extern (C):
 
+alias lxb_html_serialize_cb_f = uint function (const(lxb_char_t)* data, size_t len, void* ctx);
+alias lxb_selectors_cb_f = uint function (lxb_dom_node_t*, void*, void*);
+
+version(Windows)
+{
+    enum ToImport;
+
+    __gshared {
+        @ToImport:
+        lxb_dom_element_t* function (lxb_dom_element_t* element) lxb_dom_element_destroy;
+        lxb_dom_element_t* function (lxb_dom_document_t* document, const(lxb_dom_element_t)* element) lxb_dom_element_interface_clone;
+        lxb_dom_element_t* function (lxb_dom_element_t* element) lxb_dom_element_interface_destroy;
+        lxb_status_t function (lxb_dom_element_t* dst, const(lxb_dom_element_t)* src) lxb_dom_element_interface_copy;
+
+        lxb_dom_attr_t* function (lxb_dom_element_t* element, const(lxb_char_t)* qualified_name, size_t qn_len, const(lxb_char_t)* value, size_t value_len) lxb_dom_element_set_attribute;
+        const(lxb_char_t)* function (lxb_dom_element_t* element, const(lxb_char_t)* qualified_name, size_t qn_len, size_t* value_len) lxb_dom_element_get_attribute;
+        lxb_status_t function (lxb_dom_element_t* element, const(lxb_char_t)* qualified_name, size_t qn_len) lxb_dom_element_remove_attribute;
+        bool function (lxb_dom_element_t* element, const(lxb_char_t)* qualified_name, size_t qn_len) lxb_dom_element_has_attribute;
+
+        const(lxb_char_t)* function (lxb_dom_element_t* element, size_t* len) lxb_dom_element_tag_name;
+        const(lxb_char_t)* function (lxb_dom_element_t* element, size_t* len) lxb_dom_element_local_name;
+        lxb_dom_attr_t* function (lxb_dom_element_t* element) lxb_dom_element_first_attribute_noi;
+        lxb_dom_attr_t* function (lxb_dom_attr_t* attr) lxb_dom_element_next_attribute_noi;
+
+        void function (lxb_dom_node_t* to, lxb_dom_node_t* node) lxb_dom_node_insert_child;
+        void function (lxb_dom_node_t* to, lxb_dom_node_t* node) lxb_dom_node_insert_before;
+        void function (lxb_dom_node_t* to, lxb_dom_node_t* node) lxb_dom_node_insert_after;
+        void function (lxb_dom_node_t* node) lxb_dom_node_remove;
+
+        lxb_char_t* function (lxb_dom_node_t* node, size_t* len) lxb_dom_node_text_content;
+        lxb_status_t function (lxb_dom_node_t* node, const(lxb_char_t)* content, size_t len) lxb_dom_node_text_content_set;
+        bool function (lxb_dom_node_t* root) lxb_dom_node_is_empty;
+
+        lxb_dom_node_t* function (lxb_dom_node_t* node) lxb_dom_node_next_noi;
+        lxb_dom_node_t* function (lxb_dom_node_t* node) lxb_dom_node_prev_noi;
+        lxb_dom_node_t* function (lxb_dom_node_t* node) lxb_dom_node_parent_noi;
+        lxb_dom_node_t* function (lxb_dom_node_t* node) lxb_dom_node_first_child_noi;
+        lxb_dom_node_t* function (lxb_dom_node_t* node) lxb_dom_node_last_child_noi;
+
+        const(lxb_char_t)* function (lxb_dom_attr_t* attr, size_t* len) lxb_dom_attr_local_name_noi;
+        const(lxb_char_t)* function (lxb_dom_attr_t* attr, size_t* len) lxb_dom_attr_value_noi;
+
+        lxb_status_t function (lxb_dom_node_t* node, lxb_html_serialize_cb_f cb, void* ctx) lxb_html_serialize_cb;
+        lxb_status_t function (lxb_dom_node_t* node, lexbor_str_t* str) lxb_html_serialize_str;
+        lxb_status_t function (lxb_dom_node_t* node, lxb_html_serialize_cb_f cb, void* ctx) lxb_html_serialize_tree_cb;
+        lxb_status_t function (lxb_dom_node_t* node, lexbor_str_t* str) lxb_html_serialize_tree_str;
+        lxb_status_t function (lxb_dom_node_t* node, lxb_html_serialize_cb_f cb, void* ctx) lxb_html_serialize_deep_cb;
+        lxb_status_t function (lxb_dom_node_t* node, lexbor_str_t* str) lxb_html_serialize_deep_str;
+        lxb_status_t function (lxb_dom_node_t* node, lxb_html_serialize_opt_t opt, size_t indent, lxb_html_serialize_cb_f cb, void* ctx) lxb_html_serialize_pretty_cb;
+        lxb_status_t function (lxb_dom_node_t* node, lxb_html_serialize_opt_t opt, size_t indent, lexbor_str_t* str) lxb_html_serialize_pretty_str;
+        lxb_status_t function (lxb_dom_node_t* node, lxb_html_serialize_opt_t opt, size_t indent, lxb_html_serialize_cb_f cb, void* ctx) lxb_html_serialize_pretty_tree_cb;
+        lxb_status_t function (lxb_dom_node_t* node, lxb_html_serialize_opt_t opt, size_t indent, lexbor_str_t* str) lxb_html_serialize_pretty_tree_str;
+        lxb_status_t function (lxb_dom_node_t* node, lxb_html_serialize_opt_t opt, size_t indent, lxb_html_serialize_cb_f cb, void* ctx) lxb_html_serialize_pretty_deep_cb;
+        lxb_status_t function (lxb_dom_node_t* node, lxb_html_serialize_opt_t opt, size_t indent, lexbor_str_t* str) lxb_html_serialize_pretty_deep_str;
+
+        lxb_html_element_t* function (lxb_html_element_t* element, const(lxb_char_t)* html, size_t size) lxb_html_element_inner_html_set;
+
+        lxb_selectors_t* function () lxb_selectors_create;
+        lxb_status_t function (lxb_selectors_t* selectors) lxb_selectors_init;
+        lxb_selectors_t* function (lxb_selectors_t* selectors, bool self_destroy) lxb_selectors_destroy;
+
+        lxb_status_t function (lxb_selectors_t* selectors, lxb_dom_node_t* root, lxb_css_selector_list_t* list, lxb_selectors_cb_f cb, void* ctx) lxb_selectors_find;
+
+        lxb_css_parser_t* function() lxb_css_parser_create;
+        lxb_status_t function (lxb_css_parser_t *, lxb_css_syntax_tokenizer_t *, lexbor_mraw_t *) lxb_css_parser_init;
+        lxb_css_parser_t* function(lxb_css_parser_t *, bool) lxb_css_parser_destroy;
+
+        lxb_html_document_t* function () lxb_html_document_create;
+        void function (lxb_html_document_t* document) lxb_html_document_clean;
+        lxb_html_document_t* function (lxb_html_document_t* document) lxb_html_document_destroy;
+        lxb_status_t function (lxb_html_document_t*, const(lxb_char_t)*, size_t) lxb_html_document_parse;
+        lxb_dom_node_t* function (lxb_html_document_t*, lxb_dom_element_t*, const(lxb_char_t)*, size_t) lxb_html_document_parse_fragment;
+        const(lxb_char_t)* function (lxb_html_document_t*, size_t*) lxb_html_document_title;
+        lxb_status_t function (lxb_html_document_t*, const(lxb_char_t)*, size_t) lxb_html_document_title_set;
+        const(lxb_char_t)* function (lxb_html_document_t*, size_t*) lxb_html_document_title_raw;
+        lxb_html_head_element_t* function (lxb_html_document_t*) lxb_html_document_head_element_noi;
+        lxb_html_body_element_t* function (lxb_html_document_t*) lxb_html_document_body_element_noi;
+
+        lxb_dom_element_t* function (lxb_dom_document_t*, const(lxb_char_t)*, size_t, void*) lxb_dom_document_create_element;
+        void function(lxb_css_selector_list_t *) lxb_css_selector_list_destroy_memory;
+        lxb_css_selector_list_t * function(lxb_css_parser_t *, const lxb_char_t *, size_t) lxb_css_selectors_parse;
+    }
+
+    extern(D) shared static this()
+    {
+        import core.sys.windows.windows;
+        import std.traits;
+        import std.stdio;
+        import std.file : thisExePath;
+        import std.path : buildPath,dirName;
+        import std.string : toStringz;
+
+        HINSTANCE handle = LoadLibraryA(buildPath(thisExePath.dirName, "lexbor.dll").toStringz);
+
+        if (handle == null)
+            handle = LoadLibraryA("./lexbor.dll");
+
+        if (handle == null)
+            throw new Exception("Can't find lexbor.dll.");
+
+        static foreach(s; getSymbolsByUDA!(parserino.c.lexbor, ToImport))
+            static if (isFunctionPointer!s)
+                mixin(`*(cast(void**)&` ~ s.stringof ~ `) = cast(void*)GetProcAddress(handle, "` ~ s.stringof ~ `");`);
+    }
+}
+else
+{
+    lxb_dom_element_t* lxb_dom_element_destroy (lxb_dom_element_t* element);
+    lxb_dom_element_t* lxb_dom_element_interface_clone (lxb_dom_document_t* document, const(lxb_dom_element_t)* element);
+    lxb_dom_element_t* lxb_dom_element_interface_destroy (lxb_dom_element_t* element);
+    lxb_status_t lxb_dom_element_interface_copy (lxb_dom_element_t* dst, const(lxb_dom_element_t)* src);
+
+    lxb_dom_attr_t* lxb_dom_element_set_attribute (lxb_dom_element_t* element, const(lxb_char_t)* qualified_name, size_t qn_len, const(lxb_char_t)* value, size_t value_len);
+    const(lxb_char_t)* lxb_dom_element_get_attribute (lxb_dom_element_t* element, const(lxb_char_t)* qualified_name, size_t qn_len, size_t* value_len);
+    lxb_status_t lxb_dom_element_remove_attribute (lxb_dom_element_t* element, const(lxb_char_t)* qualified_name, size_t qn_len);
+    bool lxb_dom_element_has_attribute (lxb_dom_element_t* element, const(lxb_char_t)* qualified_name, size_t qn_len);
+
+    const(lxb_char_t)* lxb_dom_element_tag_name (lxb_dom_element_t* element, size_t* len);
+    const(lxb_char_t)* lxb_dom_element_local_name (lxb_dom_element_t* element, size_t* len);
+    lxb_dom_attr_t* lxb_dom_element_first_attribute_noi (lxb_dom_element_t* element);
+    lxb_dom_attr_t* lxb_dom_element_next_attribute_noi (lxb_dom_attr_t* attr);
+
+    void lxb_dom_node_insert_child (lxb_dom_node_t* to, lxb_dom_node_t* node);
+    void lxb_dom_node_insert_before (lxb_dom_node_t* to, lxb_dom_node_t* node);
+    void lxb_dom_node_insert_after (lxb_dom_node_t* to, lxb_dom_node_t* node);
+    void lxb_dom_node_remove (lxb_dom_node_t* node);
+
+    lxb_char_t* lxb_dom_node_text_content (lxb_dom_node_t* node, size_t* len);
+    lxb_status_t lxb_dom_node_text_content_set (lxb_dom_node_t* node, const(lxb_char_t)* content, size_t len);
+    bool lxb_dom_node_is_empty (lxb_dom_node_t* root);
+
+    lxb_dom_node_t* lxb_dom_node_next_noi (lxb_dom_node_t* node);
+    lxb_dom_node_t* lxb_dom_node_prev_noi (lxb_dom_node_t* node);
+    lxb_dom_node_t* lxb_dom_node_parent_noi (lxb_dom_node_t* node);
+    lxb_dom_node_t* lxb_dom_node_first_child_noi (lxb_dom_node_t* node);
+    lxb_dom_node_t* lxb_dom_node_last_child_noi (lxb_dom_node_t* node);
+
+    const(lxb_char_t)* lxb_dom_attr_local_name_noi (lxb_dom_attr_t* attr, size_t* len);
+    const(lxb_char_t)* lxb_dom_attr_value_noi (lxb_dom_attr_t* attr, size_t* len);
+
+    lxb_status_t lxb_html_serialize_cb (lxb_dom_node_t* node, lxb_html_serialize_cb_f cb, void* ctx);
+    lxb_status_t lxb_html_serialize_str (lxb_dom_node_t* node, lexbor_str_t* str);
+    lxb_status_t lxb_html_serialize_tree_cb (lxb_dom_node_t* node, lxb_html_serialize_cb_f cb, void* ctx);
+    lxb_status_t lxb_html_serialize_tree_str (lxb_dom_node_t* node, lexbor_str_t* str);
+    lxb_status_t lxb_html_serialize_deep_cb (lxb_dom_node_t* node, lxb_html_serialize_cb_f cb, void* ctx);
+    lxb_status_t lxb_html_serialize_deep_str (lxb_dom_node_t* node, lexbor_str_t* str);
+    lxb_status_t lxb_html_serialize_pretty_cb (lxb_dom_node_t* node, lxb_html_serialize_opt_t opt, size_t indent, lxb_html_serialize_cb_f cb, void* ctx);
+    lxb_status_t lxb_html_serialize_pretty_str (lxb_dom_node_t* node, lxb_html_serialize_opt_t opt, size_t indent, lexbor_str_t* str);
+    lxb_status_t lxb_html_serialize_pretty_tree_cb (lxb_dom_node_t* node, lxb_html_serialize_opt_t opt, size_t indent, lxb_html_serialize_cb_f cb, void* ctx);
+    lxb_status_t lxb_html_serialize_pretty_tree_str (lxb_dom_node_t* node, lxb_html_serialize_opt_t opt, size_t indent, lexbor_str_t* str);
+    lxb_status_t lxb_html_serialize_pretty_deep_cb (lxb_dom_node_t* node, lxb_html_serialize_opt_t opt, size_t indent, lxb_html_serialize_cb_f cb, void* ctx);
+    lxb_status_t lxb_html_serialize_pretty_deep_str (lxb_dom_node_t* node, lxb_html_serialize_opt_t opt, size_t indent, lexbor_str_t* str);
+
+    lxb_html_element_t* lxb_html_element_inner_html_set (lxb_html_element_t* element, const(lxb_char_t)* html, size_t size);
+
+    lxb_selectors_t* lxb_selectors_create ();
+    lxb_status_t lxb_selectors_init (lxb_selectors_t* selectors);
+    lxb_selectors_t* lxb_selectors_destroy (lxb_selectors_t* selectors, bool self_destroy);
+
+    lxb_status_t lxb_selectors_find (lxb_selectors_t* selectors, lxb_dom_node_t* root, lxb_css_selector_list_t* list, lxb_selectors_cb_f cb, void* ctx);
+
+    lxb_css_parser_t* lxb_css_parser_create();
+    lxb_status_t lxb_css_parser_init(lxb_css_parser_t *, lxb_css_syntax_tokenizer_t *, lexbor_mraw_t *);
+    lxb_css_parser_t *lxb_css_parser_destroy(lxb_css_parser_t *, bool);
+
+    lxb_html_document_t* lxb_html_document_create ();
+    void lxb_html_document_clean (lxb_html_document_t* document);
+    lxb_html_document_t* lxb_html_document_destroy (lxb_html_document_t* document);
+    lxb_status_t lxb_html_document_parse (lxb_html_document_t*, const(lxb_char_t)*, size_t);
+    lxb_dom_node_t* lxb_html_document_parse_fragment (lxb_html_document_t*, lxb_dom_element_t*, const(lxb_char_t)*, size_t);
+    const(lxb_char_t)* lxb_html_document_title (lxb_html_document_t*, size_t*);
+    lxb_status_t lxb_html_document_title_set (lxb_html_document_t*, const(lxb_char_t)*, size_t);
+    const(lxb_char_t)* lxb_html_document_title_raw (lxb_html_document_t*, size_t*);
+    lxb_html_head_element_t* lxb_html_document_head_element_noi (lxb_html_document_t*);
+    lxb_html_body_element_t* lxb_html_document_body_element_noi (lxb_html_document_t*);
+
+    lxb_dom_element_t* lxb_dom_document_create_element (lxb_dom_document_t*, const(lxb_char_t)*, size_t, void*);
+    void lxb_css_selector_list_destroy_memory(lxb_css_selector_list_t *);
+    lxb_css_selector_list_t * lxb_css_selectors_parse(lxb_css_parser_t *, const lxb_char_t *, size_t);
+}
+
+// STRUCTS
 alias lxb_dom_attr_id_t = uintptr_t;
 alias lxb_char_t = ubyte;
 alias lxb_status_t = uint;
 alias lxb_html_document_opt_t = uint;
 alias lxb_html_serialize_opt_t = int;
-
-lxb_dom_element_t* lxb_dom_element_destroy (lxb_dom_element_t* element);
-lxb_dom_element_t* lxb_dom_element_interface_clone (lxb_dom_document_t* document, const(lxb_dom_element_t)* element);
-lxb_dom_element_t* lxb_dom_element_interface_destroy (lxb_dom_element_t* element);
-lxb_status_t lxb_dom_element_interface_copy (lxb_dom_element_t* dst, const(lxb_dom_element_t)* src);
-
-lxb_dom_attr_t* lxb_dom_element_set_attribute (lxb_dom_element_t* element, const(lxb_char_t)* qualified_name, size_t qn_len, const(lxb_char_t)* value, size_t value_len);
-const(lxb_char_t)* lxb_dom_element_get_attribute (lxb_dom_element_t* element, const(lxb_char_t)* qualified_name, size_t qn_len, size_t* value_len);
-lxb_status_t lxb_dom_element_remove_attribute (lxb_dom_element_t* element, const(lxb_char_t)* qualified_name, size_t qn_len);
-bool lxb_dom_element_has_attribute (lxb_dom_element_t* element, const(lxb_char_t)* qualified_name, size_t qn_len);
-
-const(lxb_char_t)* lxb_dom_element_tag_name (lxb_dom_element_t* element, size_t* len);
-const(lxb_char_t)* lxb_dom_element_local_name (lxb_dom_element_t* element, size_t* len);
-lxb_dom_attr_t* lxb_dom_element_first_attribute_noi (lxb_dom_element_t* element);
-lxb_dom_attr_t* lxb_dom_element_next_attribute_noi (lxb_dom_attr_t* attr);
-
-void lxb_dom_node_insert_child (lxb_dom_node_t* to, lxb_dom_node_t* node);
-void lxb_dom_node_insert_before (lxb_dom_node_t* to, lxb_dom_node_t* node);
-void lxb_dom_node_insert_after (lxb_dom_node_t* to, lxb_dom_node_t* node);
-void lxb_dom_node_remove (lxb_dom_node_t* node);
-
-lxb_char_t* lxb_dom_node_text_content (lxb_dom_node_t* node, size_t* len);
-lxb_status_t lxb_dom_node_text_content_set (lxb_dom_node_t* node, const(lxb_char_t)* content, size_t len);
-bool lxb_dom_node_is_empty (lxb_dom_node_t* root);
-
-lxb_dom_node_t* lxb_dom_node_next_noi (lxb_dom_node_t* node);
-lxb_dom_node_t* lxb_dom_node_prev_noi (lxb_dom_node_t* node);
-lxb_dom_node_t* lxb_dom_node_parent_noi (lxb_dom_node_t* node);
-lxb_dom_node_t* lxb_dom_node_first_child_noi (lxb_dom_node_t* node);
-lxb_dom_node_t* lxb_dom_node_last_child_noi (lxb_dom_node_t* node);
-
-const(lxb_char_t)* lxb_dom_attr_local_name_noi (lxb_dom_attr_t* attr, size_t* len);
-const(lxb_char_t)* lxb_dom_attr_value_noi (lxb_dom_attr_t* attr, size_t* len);
-
-alias lxb_html_serialize_cb_f = uint function (const(lxb_char_t)* data, size_t len, void* ctx);
-lxb_status_t lxb_html_serialize_cb (lxb_dom_node_t* node, lxb_html_serialize_cb_f cb, void* ctx);
-lxb_status_t lxb_html_serialize_str (lxb_dom_node_t* node, lexbor_str_t* str);
-lxb_status_t lxb_html_serialize_tree_cb (lxb_dom_node_t* node, lxb_html_serialize_cb_f cb, void* ctx);
-lxb_status_t lxb_html_serialize_tree_str (lxb_dom_node_t* node, lexbor_str_t* str);
-lxb_status_t lxb_html_serialize_deep_cb (lxb_dom_node_t* node, lxb_html_serialize_cb_f cb, void* ctx);
-lxb_status_t lxb_html_serialize_deep_str (lxb_dom_node_t* node, lexbor_str_t* str);
-lxb_status_t lxb_html_serialize_pretty_cb (lxb_dom_node_t* node, lxb_html_serialize_opt_t opt, size_t indent, lxb_html_serialize_cb_f cb, void* ctx);
-lxb_status_t lxb_html_serialize_pretty_str (lxb_dom_node_t* node, lxb_html_serialize_opt_t opt, size_t indent, lexbor_str_t* str);
-lxb_status_t lxb_html_serialize_pretty_tree_cb (lxb_dom_node_t* node, lxb_html_serialize_opt_t opt, size_t indent, lxb_html_serialize_cb_f cb, void* ctx);
-lxb_status_t lxb_html_serialize_pretty_tree_str (lxb_dom_node_t* node, lxb_html_serialize_opt_t opt, size_t indent, lexbor_str_t* str);
-lxb_status_t lxb_html_serialize_pretty_deep_cb (lxb_dom_node_t* node, lxb_html_serialize_opt_t opt, size_t indent, lxb_html_serialize_cb_f cb, void* ctx);
-lxb_status_t lxb_html_serialize_pretty_deep_str (lxb_dom_node_t* node, lxb_html_serialize_opt_t opt, size_t indent, lexbor_str_t* str);
-
-lxb_html_element_t* lxb_html_element_inner_html_set (lxb_html_element_t* element, const(lxb_char_t)* html, size_t size);
-
-alias lxb_selectors_cb_f = uint function (lxb_dom_node_t*, void*, void*);
-
-lxb_selectors_t* lxb_selectors_create ();
-lxb_status_t lxb_selectors_init (lxb_selectors_t* selectors);
-lxb_selectors_t* lxb_selectors_destroy (lxb_selectors_t* selectors, bool self_destroy);
-
-lxb_status_t lxb_selectors_find (lxb_selectors_t* selectors, lxb_dom_node_t* root, lxb_css_selector_list_t* list, lxb_selectors_cb_f cb, void* ctx);
-
-lxb_css_parser_t* lxb_css_parser_create();
-lxb_status_t lxb_css_parser_init(lxb_css_parser_t *, lxb_css_syntax_tokenizer_t *, lexbor_mraw_t *);
-lxb_css_parser_t *lxb_css_parser_destroy(lxb_css_parser_t *, bool);
-
-lxb_html_document_t* lxb_html_document_create ();
-void lxb_html_document_clean (lxb_html_document_t* document);
-lxb_html_document_t* lxb_html_document_destroy (lxb_html_document_t* document);
-lxb_status_t lxb_html_document_parse (lxb_html_document_t*, const(lxb_char_t)*, size_t);
-lxb_dom_node_t* lxb_html_document_parse_fragment (lxb_html_document_t*, lxb_dom_element_t*, const(lxb_char_t)*, size_t);
-const(lxb_char_t)* lxb_html_document_title (lxb_html_document_t*, size_t*);
-lxb_status_t lxb_html_document_title_set (lxb_html_document_t*, const(lxb_char_t)*, size_t);
-const(lxb_char_t)* lxb_html_document_title_raw (lxb_html_document_t*, size_t*);
-lxb_html_head_element_t* lxb_html_document_head_element_noi (lxb_html_document_t*);
-lxb_html_body_element_t* lxb_html_document_body_element_noi (lxb_html_document_t*);
-
-lxb_dom_element_t* lxb_dom_document_create_element (lxb_dom_document_t*, const(lxb_char_t)*, size_t, void*);
-void lxb_css_selector_list_destroy_memory(lxb_css_selector_list_t *);
-lxb_css_selector_list_t * lxb_css_selectors_parse(lxb_css_parser_t *, const lxb_char_t *, size_t);
-
-// STRUCTS
 
 struct lxb_selectors_t;
 struct lxb_css_selector_list_t;
